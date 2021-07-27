317da06851a496eebcf4653cf1c8e5c4108cb35d
edgar martinez <edgar@space.bar>
2021-07-27 14:25:30 -0600

    ok

--- app/services/dashboard/main_dashboard.rb
+++ app/services/dashboard/main_dashboard.rb
@@ -11,40 +11,36 @@ module Dashboard
         private 
         def dashboard
             type = @params[:user_type]
+            user_information = rescue_user_information(@params[:id])[0]
             if type == "admin_ampip"
-                return {"developers":rescue_dev(0), "sponsors":rescue_sponsors(0), "user_information":rescue_user_information(@params[:id])}
+                return {"developers":rescue_corporate(0, 1), "sponsors":rescue_corporate(0, 0), "user_information":user_information}
             elsif type == "user_ampip"
-                return {"developers":rescue_dev(0), "sponsors":rescue_sponsors(0), "user_information":rescue_user_information(@params[:id])}
+                return {"developers":rescue_corporate(0, 1), "sponsors":rescue_corporate(0, 0), "user_information":user_information}
+            elsif type == "admin_society"
+                return {"developers":rescue_corporate(user_information), "user_information":user_information}
             end
         end
 
-        #rescata los o el patrocinador dependiendo de el requerimiento
-        def rescue_sponsors(id)
+        #rescata los o el desarrollador dependiendo de el requerimiento donde type  (0 => patrocinadores o 1 => desarrolladores)
+        def rescue_corporate(id, type = nil)
             if id == 0
-                return Corporate.where(corporate_type: 0)
+                return Corporate.where(corporate_type: type)
             else
-                return Corporate.where(id:id)
-            end            
+                if id == "Sin datos"
+                    return "Sin Desarrollador asignado"
+                else 
+                    return Corporate.where(id: id.corporate_id)
+                end    
+            end
         end
-
-        #rescata los o el desarrollador dependiendo de el requerimiento
-        def rescue_dev(id)
-            if id == 0
-                return Corporate.where(corporate_type: 1)
-            else
-                return Corporate.where(id:id)
-            end            
-        end
-
         #obtiene la informacion del usuario
         def rescue_user_information(id)
             @user = UserInformation.where(user_id:id)
             if @user != []
                 return @user
             else
-                return "Sin datos aun"
+                return ["Sin datos"]
             end
-            
         end      
     end 
 end
